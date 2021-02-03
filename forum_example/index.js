b14b2947325fad68c9261525a2c7c02658ba3e01
Philip Huang <philip.sw.huang@gmail.com>
2021-02-02 16:19:39 -0800

    CRUD and REST completed

--- forum_example/index.js
+++ forum_example/index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const path = require('path');
+const { v4: newID } = require('uuid');
+const methodOverride = require('method-override');
 
 const port = 3000;
 const app = express();
 
 let posts = [
   {
+    id: newID(),
     author: "Andrew",
     comment: "This is my comment"
   },
   {
+    id: newID(),
     author: "Phil",
     comment: "I'm the best"
   },
   {
+    id: newID(),
     author: "Raina",
     comment: "I don't read the rules"
   }
@@ -23,6 +28,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   // const { author, comment } = req.body;
@@ -34,13 +40,47 @@ app.get('/new', (req, res) => {
   res.render('new');
 })
 
-app.get('/home', (req, res) => {
-  res.send('Get /new response');
-})
-
 app.post('/home', (req, res) => {
   console.log(req.body);
-  res.send('It worked!')
+  const { author, comment } = req.body;
+  posts.push({ author, comment, id: newID() })
+  res.redirect('/')
+})
+
+app.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const comment = posts.find(c => c.id === id);
+  console.log(comment)
+  if (comment) {
+    res.render('show', { comment })
+  } else {
+    res.render('/');
+  }
+})
+
+app.get('/:id/edit', (req, res) => {
+  const { id } = req.params;
+  const comment = posts.find(c => c.id === id);
+  if (comment) {
+    res.render('edit', { comment })
+  } else {
+    res.render('/')
+  }
+})
+
+app.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const comment = posts.find(c => c.id === id);
+  const updatedComment = req.body.comment;
+  comment.comment = updatedComment
+  res.redirect('/')
+  // res.send('hi!')
+})
+
+app.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  posts = posts.filter(c => c.id !== id);
+  res.redirect('/')
 })
 
 app.listen(3000, () => {
