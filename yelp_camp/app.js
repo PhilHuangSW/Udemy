2f2d2e80a392a4403b22ada1623d5341254903bc
Philip Huang <philip.sw.huang@gmail.com>
2021-02-10 11:46:48 -0800

    Included Bootstrap, EJS-Mate, Partials/Layouts

--- yelp_camp/app.js
+++ yelp_camp/app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const morgan = require('morgan');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -17,11 +19,14 @@ db.once('open', () => {
 })
 
 const app = express();
+
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
   res.render('home');
