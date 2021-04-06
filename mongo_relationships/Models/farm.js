07ae02419c29bb619a397e839c676e996df5415e
Philip Huang <philip.sw.huang@gmail.com>
2021-04-05 18:17:45 -0700

    Added reviews to yelp-camp

--- mongo_relationships/Models/farm.js
+++ mongo_relationships/Models/farm.js
@@ -1,60 +1,37 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-mongoose.connect('mongodb://localhost:27017/relationshipDemo', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Mongo Connection Opened!");
-  })
-  .catch(err => {
-    console.log("Mongo Connection Error Occurred!")
-    console.log(err)
-  });
-
-const productSchema = new Schema({
-  name: String,
-  price: Number,
-  season: {
-    type: String,
-    enum: ['Spring', 'Summer', 'Fall', 'Winter']
-  }
-})
+const Product = require('./product');
+const { Schema } = mongoose;
 
 const farmSchema = new Schema({
-  name: String,
-  city: String,
-  products: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Product'
-  }]
-})
+    name: {
+        type: String,
+        required: [true, 'Farm must have a name!']
+    },
+    city: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: [true, 'Email required']
+    },
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    ]
+});
 
-const Product = mongoose.model('Product', productSchema);
+// DELETE ALL ASSOCIATED PRODUCTS AFTER A FARM IS DELETED
+// farmSchema.post('findOneAndDelete', async function (farm) {
+//     if (farm.products.length) {
+//         const res = await Product.deleteMany({ _id: { $in: farm.products } })
+//         console.log(res);
+//     }
+// })
+
 const Farm = mongoose.model('Farm', farmSchema);
 
-// Product.insertMany([
-//   { name: 'Goddess Melon', price: 4.99, season: 'Summer' },
-//   { name: 'Sugar Baby Watermelon', price: 4.99, season: 'Summer' },
-//   { name: 'Asparagus', price: 3.99, season: 'Spring' }
-// ])
 
-// const makeFarm = async () => {
-//   const farm = new Farm({ name: 'Full Belly Farms', city: 'Guinda, CA' });
-//   const melon = await Product.findOne({ name: 'Goddess Melon' });
-//   farm.products.push(melon);
-//   console.log(farm);
-//   await farm.save();
-// }
 
-// makeFarm();
-
-const addProduct = async () => {
-  const farm = await Farm.findOne({ name: 'Full Belly Farms' });
-  const watermelon = await Product.findOne({ name: 'Sugar Baby Watermelon' });
-  farm.products.push(watermelon);
-  farm.save();
-}
-
-// addProduct();
-Farm.findOne({ name: 'Full Belly Farms' })
-  .populate('products')
-  .then(farm => console.log(farm))
+module.exports = Farm;
--- yelp_camp/app.js
+++ yelp_camp/app.js
@@ -7,7 +7,8 @@ const morgan = require('morgan');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
 const catchAsync = require('./utils/catchAsync');
-const { campgroundSchema } = require('./schemas');
+const { campgroundSchema, reviewSchema } = require('./schemas');
+const Review = require('./models/review')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -41,6 +42,16 @@ const validateCampground = (req, res, next) => {
   }
 }
 
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  } else {
+    next();
+  }
+}
+
 app.get('/', (req, res) => {
   res.render('home');
 })
@@ -62,7 +73,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
-  const campground = await Campground.findById(id);
+  const campground = await Campground.findById(id).populate('reviews');
   res.render('campgrounds/show', { campground });
 }))
 
@@ -88,6 +99,22 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndDelete(id)
   res.redirect('/campgrounds');
+}))
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+  const { id, reviewId } = req.params;
+  Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/campgrounds/${id}`);
 }))
 
 app.all('*', (req, res, next) => {
