72fe7b2113904d5b138eb67180022e77b5e91a4e
Philip Huang <philip.sw.huang@gmail.com>
2021-04-07 14:15:27 -0700

    Cookies Demo Complete

--- CookiesDemo/index.js
+++ CookiesDemo/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 
+app.use(cookieParser('thisismysecret'));
+
 app.get('/greet', (req, res) => {
-  res.send('Hello!')
+  const { name = "I don't think I know your name!" } = req.cookies;
+  res.send(`Hello ${name}`)
 })
 
+app.get('/setname', (req, res) => {
+  res.cookie('name', 'Phil');
+  res.send('Okay, sent you a cookie!');
+})
+
+app.get('/getsignedcookie', (req, res) => {
+  res.cookie('fruit', 'grape', { signed: true });
+  res.send('signed your fruit cookie!');
+})
+
+app.get('/verifyfruit', (req, res) => {
+  res.send(req.signedCookies);
+})
+
+app.listen(3000, () => {
+  console.log('listening on localhost 3000');
+})
