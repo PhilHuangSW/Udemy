c4eae09342e88f3c81a15f0b025de65204340569
Philip Huang <philip.sw.huang@gmail.com>
2021-04-07 18:59:03 -0700

    Added Routers to yelp-camp

---
+++ yelp_camp/routes/campgrounds.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const ExpressError = require('../utils/ExpressError');
+const catchAsync = require('../utils/catchAsync');
+const Review = require('../models/review');
+const Campground = require('../models/campground');
+const { campgroundSchema } = require('../schemas');
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  } else {
+    next();
+  }
+}
+
+router.get('/', catchAsync(async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
+}))
+
+router.get('/new', (req, res) => {
+  res.render('campgrounds/new');
+})
+
+router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+router.get('/:id', catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id).populate('reviews');
+  res.render('campgrounds/show', { campground });
+}))
+
+router.get('/:id/edit', catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render('campgrounds/edit', { campground });
+}))
+
+router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+  const { id } = req.params;
+  // console.log(title)
+  // console.log(location)
+  console.log(req.body);
+  const campground = await Campground.findByIdAndUpdate(id, req.body.campground, { new: true });
+  // campground.title = title;
+  // campground.location = location;
+  // await campground.save()
+  res.redirect(`/campgrounds/${campground.id}`);
+}))
+
+router.delete('/:id', catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndDelete(id)
+  res.redirect('/campgrounds');
+}))
+
+module.exports = router;
