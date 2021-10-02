6242ade8aabebd054ec19d558b2acec8f30426b4
Philip Huang <philip.sw.huang@gmail.com>
2021-10-01 21:12:34 -0700

    Updated seed files to prepare seeding entries

--- grounds/seeds/index.js
+++ grounds/seeds/index.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const campground = require('../models/campground');
 const Campground = require('../models/campground');
 const cities = require('./cities');
+const authors = require('./authors');
 const { places, descriptors } = require('./seedHelpers');
+const imageFiles = require('./imageFiles');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -19,12 +21,14 @@ db.once('open', () => {
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-  await Campground.deleteMany({});
-  for (let i = 0; i < 300; i++) {
+  // await Campground.deleteMany({});
+  for (let i = 0; i < 2; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
-    const price = Math.floor(Math.random() * 20) + 10;
+    const random24 = Math.floor(Math.random() * 24);
+    const random35 = Math.floor(Math.random() * 35) + 1;
+    const price = Math.floor(Math.random() * 30) + 10;
     const camp = new Campground({
-      author: '607fbe6d8cb5ae15591b99c7',
+      author: `${authors[random24]}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       geometry: {
@@ -36,12 +40,8 @@ const seedDB = async () => {
       },
       images: [
         {
-          url: 'https://res.cloudinary.com/dlkcz7vpy/image/upload/v1619370163/YelpCamp/pdhhe58lmpat2nw2ibtl.jpg',
-          filename: 'YelpCamp/pdhhe58lmpat2nw2ibtl'
-        },
-        {
-          url: 'https://res.cloudinary.com/dlkcz7vpy/image/upload/v1619370163/YelpCamp/t1sbq5piqshe0uxfzaux.jpg',
-          filename: 'YelpCamp/t1sbq5piqshe0uxfzaux'
+          url: `${imageFiles.url}`,
+          filename: `YelpCamp/${imageFiles.filename}`
         }
       ],
       description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores optio amet illum ab perferendis! Non, atque, laboriosam itaque voluptas quam perspiciatis animi quidem eum optio eaque fuga magni fugiat inventore!',
